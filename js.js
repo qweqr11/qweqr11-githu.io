62ce96064fe4c74e5f0125c6ef089da7cfafe2c0
qweqr11 <138442436+qweqr11@users.noreply.github.com>
2023-09-30 15:13:24 +0800

    Update js.js

--- js.js
+++ js.js
@@ -84,7 +84,7 @@ function updateV(e){
 
 const cash = document.getElementById('cash')
 cash.addEventListener('click',function(){
-    if tg.MainButton.isVisible{
+    if (tg.MainButton.isVisible) {
         tg.MainButton.hide();
     }else{
         tg.Button.setText("Вы нажали пополнить");
